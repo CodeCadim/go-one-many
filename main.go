d842cf7283f3d12390f77af0306ed4f8145bf953
Brahim Hamdouni <brahim@hamdouni.com>
2023-09-07 14:51:05 +0200

    add some comments

--- main.go
+++ main.go
@@ -9,13 +9,15 @@ import (
 )
 
 func main() {
-	println("Hello")
+	// Connect to the local demo database provided via docker
 	db, err := sqlx.Connect("mysql", "root@tcp(localhost:3306)/demo")
 	if err != nil {
 		log.Fatalln(err)
 	}
 	defer db.Close()
 
+	// Our simple business model with 2 data-structures and the
+	// one-to-many relation : 1 author can have many posts.
 	type Post struct {
 		ID    int64
 		Title string
@@ -26,14 +28,19 @@ func main() {
 		Posts []Post
 	}
 
+	// We provide an adhoc structure to collect the database results
 	dest := []struct {
 		ID        int64  `db:"a_id"`
 		Name      string `db:"a_name"`
 		PostID    int64  `db:"p_id"`
 		PostTitle string `db:"p_title"`
 	}{}
-	set := map[int64]*Author{}
 	db.Select(&dest, "SELECT a_id, a_name, p_id, p_title FROM authors LEFT JOIN posts ON p_author=a_id")
+
+	// The result contains duplicate of author's datas, so we deduplicate
+	// them with a map and an iteration on each result to group the posts
+	// in the author's Posts field
+	set := map[int64]*Author{}
 	for _, item := range dest {
 		auth, ok := set[item.ID]
 		if !ok {
@@ -46,9 +53,13 @@ func main() {
 			auth.Posts = append(auth.Posts, Post{ID: item.PostID, Title: item.PostTitle})
 		}
 	}
+
+	// Then we reassemble a valid business data-structure from our map
 	var res []Author
 	for _, v := range set {
 		res = append(res, *v)
 	}
+
+	// And that's it !
 	fmt.Printf("%+v\n", res)
 }
